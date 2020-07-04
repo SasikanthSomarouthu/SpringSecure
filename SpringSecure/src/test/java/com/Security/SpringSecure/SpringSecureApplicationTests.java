038c88fb53226104d3569fe7a3680470e893e8f7
SasikanthSomarouthu <somarouthusasikanth@gmail.com>
2020-07-04 13:20:45 -0400

    intialization

---
+++ SpringSecure/src/test/java/com/Security/SpringSecure/SpringSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Security.SpringSecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
